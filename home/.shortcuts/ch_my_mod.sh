6fadad5cd30745e421a8369f134950ed372b9172
秦Daniel <daniel_chin@yahoo.com>
2019-10-01 23:25:56 -0400

    ch_my_mod

---
+++ home/.shortcuts/ch_my_mod.sh
@@ -0,0 +1,6 @@
+chmod +x ./cd_sdcard.sh
+chmod +x ./pad.sh
+chmod +x ./pull.sh
+chmod +x ./python.sh
+chmod +x ./Termux.sh
+chmod +x ./WhereDitGo.sh
---
+++ usr/bin/ch_my_mod.sh
@@ -0,0 +1,5 @@
+chmod +x ./daniel_setup.sh
+chmod +x ./pad.sh
+chmod +x ./pull.sh
+chmod +x ./py.sh
+chmod +x ./pym.sh
